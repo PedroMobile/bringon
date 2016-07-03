5ed56e2b25d2552d64ad58c836d3546010b07b2d
Pedro Henrique <Pedro Henrique>
2016-07-03 12:10:47 -0300

    did add form my account

--- app/src/main/java/br/com/pedrosousa/bringon/fragment/MyAccountFragment.java
+++ app/src/main/java/br/com/pedrosousa/bringon/fragment/MyAccountFragment.java
@@ -1,11 +1,16 @@
 package br.com.pedrosousa.bringon.fragment;
 
 import android.os.Bundle;
+import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+
+import java.lang.ref.WeakReference;
 
 import br.com.pedrosousa.bringon.R;
+import br.com.pedrosousa.bringon.utils.BrPhoneNumberFormatter;
 import butterknife.ButterKnife;
 
 /**
@@ -18,12 +23,16 @@ public class MyAccountFragment extends BaseNavFragment{
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_my_account, container, false);
         unbinder = ButterKnife.bind(this, v);
-        init();
+        init(v);
         return v;
     }
 
-    private void init(){
+    private void init(View v){
         getNavActivity().getSupportActionBar().setTitle(R.string.my_account);
+
+        EditText editPhone = (EditText) v.findViewById(R.id.edit_phone);
+        BrPhoneNumberFormatter addLineNumberFormatter = new BrPhoneNumberFormatter(new WeakReference<EditText>(editPhone));
+        editPhone.addTextChangedListener(addLineNumberFormatter);
 
     }
 }
